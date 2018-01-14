7e4c393a9527097e8fc2f96e3985137dab9ba246
casual_ji <casual_ji@hotmail.com>
2018-01-14 18:39:53 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,177 @@
+//index.js
+//获取应用实例
+var app = getApp();
+var order = ['red', 'yellow', 'blue', 'green', 'red']
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    toView: 'red',
+    scrollTop: 100,
+    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+    name: '此时此刻',
+    author: '许巍',
+    src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    cores: [
+      [
+        { id: 'kb', name: '课表查询', disabled: false, teacher_disabled: false, offline_disabled: false },
+        { id: 'cj', name: '成绩查询', disabled: false, teacher_disabled: true, offline_disabled: false },
+        { id: 'ks', name: '考试安排', disabled: false, teacher_disabled: false, offline_disabled: false },
+        { id: 'kjs', name: '空教室', disabled: false, teacher_disabled: false, offline_disabled: true },
+        { id: 'xs', name: '学生查询', disabled: false, teacher_disabled: false, offline_disabled: true },
+        { id: 'ykt', name: '一卡通', disabled: false, teacher_disabled: false, offline_disabled: false },
+        { id: 'jy', name: '借阅信息', disabled: false, teacher_disabled: false, offline_disabled: false },
+        { id: 'xf', name: '学费信息', disabled: false, teacher_disabled: true, offline_disabled: false },
+        { id: 'sdf', name: '电费查询', disabled: false, teacher_disabled: true, offline_disabled: false },
+        { id: 'bx', name: '物业报修', disabled: false, teacher_disabled: false, offline_disabled: true }
+      ], [
+        { id: 'cet', name: '四六级', disabled: false, teacher_disabled: true, offline_disabled: true },
+        { id: 'fw', name: "志愿活动", disabled: false, teacher_disabled: true, offline_disabled: false }
+      ]
+    ],
+    card: {
+      'kb': {
+        show: false,
+        time_list: [
+          { begin: '8:00', end: '8:45' },
+          { begin: '8:55', end: '9:40' },
+          { begin: '10:05', end: '10:50' },
+          { begin: '11:00', end: '11:45' },
+          { begin: '14:00', end: '14:45' },
+          { begin: '14:55', end: '15:40' },
+          { begin: '16:05', end: '16:50' },
+          { begin: '17:00', end: '17:45' },
+          { begin: '19:00', end: '19:45' },
+          { begin: '19:55', end: '20:40' },
+          { begin: '20:50', end: '21:35' },
+          { begin: '21:45', end: '22:30' }
+        ],
+        data: {}
+      },
+      'kb': {
+        show: true,
+        nothing:true,
+        data: {
+          'last_time': '',
+          'balance': 0,
+          'cost_status': false,
+          'today_cost': {
+            value: [],
+            total: 0
+          }
+        }
+      },
+      'ykt': {
+        show: false,
+        data: {
+          'last_time': '',
+          'balance': 0,
+          'cost_status': false,
+          'today_cost': {
+            value: [],
+            total: 0
+          }
+        }
+      },
+      'jy': {
+        show: true,
+        data: {}
+      },
+      'sdf': {
+        show: false,
+        data: {
+          'room': '',
+          'record_time': '',
+          'cost': 0,
+          'spend': 0
+        }
+      }
+    },
+    user: {},
+    disabledItemTap: false //点击了不可用的页面
+  },
+ 
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function (e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
+})
---
+++ pages/login/login.js
@@ -0,0 +1,97 @@
+Page({
+  data: {
+    inputPassword: false,
+    isLoading: false,
+    password: '',
+    username: ''
+  },
+  onLoad: function () {
+    wx.getStorage({
+      key: 'account',
+      success: function (res) {
+        if(res.data){
+          console.log('用户已登录')
+          wx.navigateTo({
+            url: '/pages/index/index',
+          })
+        }
+        
+        
+      
+      }
+    })
+  },
+  pwdFocus() {
+    this.setData({
+      inputPassword: true
+    })
+  },
+  formSubmit: function (e) {
+    this.setData({
+      isLoading: true
+    })
+    //console.log(e.detail.value)
+    //获得表单数据
+    var objData = e.detail.value;
+    if (objData.username && objData.password) {
+      wx.request({
+        url: 'https://guohe3.com/api/studentInfo',
+        method	:'POST',
+        data: {
+          username: objData.username ,
+          password: objData.password
+        },
+        header: {
+          'content-type': 'application/x-www-form-urlencoded' // 默认值
+        },
+        success: function (res) {
+          //console.log(res.data)
+          if(res.data.code==200){
+            //登录成功
+            wx.showToast({
+              title: '登录成功',
+            })
+            var result=res.data.info
+            result.password= objData.password
+            console.log(result)
+            wx.setStorage({
+              key: "account",
+              data: result
+            })
+            wx.reLaunch({
+              url: '/pages/index/index',
+            })  
+           
+          }else{
+
+             //登录失败
+             console.log('登录失败')
+             wx.showToast({
+               title: '登录失败',
+               icon:'loading',
+               duration: 2000
+             })
+          }
+        }
+      })
+    } else {
+      wx.showToast({
+        title: '学号或密码不能为空',
+        icon: 'success',
+        duration: 2000
+      })
+    }
+
+
+
+
+
+    setTimeout(() => {
+      this.setData({
+        isLoading: false
+      })
+    }, 1000)
+
+  }
+
+})
