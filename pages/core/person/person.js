213dbe2c02f7ee6ea07ba90e20f958f9ab297bd4
Ethan <2955317305@qq.com>
2018-02-26 14:34:42 +0800

    解决缓存问题

--- pages/core/person/person.js
+++ pages/core/person/person.js
@@ -15,7 +15,7 @@ Page({
           wx.navigateTo({
             url: '/pages/login/login',
           })
-        } else if (res.cancel) {
+        } else if (res.cancel) {  
           console.log('用户点击取消')
         }
       }
@@ -35,6 +35,7 @@ Page({
      })
     //调用应用实例的方法获取全局数据  
     app.getUserInfo(function (userInfo) {
+      console.log(userInfo)
       //更新数据  
       that.setData({
         userInfo: userInfo
